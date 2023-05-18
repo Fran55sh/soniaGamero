af1648d99b6ffe484e1fe6ea050b49f46f96dd27
fran55sh <corvattafranco@gmail.com>
2023-05-18 16:50:30 -0300

    add db and create prop functionaliity

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,56 @@ router.post('/acceso-restringido', (req, res) => {
   }
 });
 
+
+const express = require('express');
+const { Propiedad, Tipo, Condicion, sequelize } = require('./ruta-de-tu-modelo');
+
+const app = express();
+app.use(express.json());
+
+// Ruta para crear una nueva propiedad
+app.post('/crear-propiedad', async (req, res) => {
+  try {
+    const {
+      nombre,
+      descripcion,
+      precio,
+      esDestacado,
+      foto,
+      tipo,
+      condicion,
+    } = req.body;
+
+    // // Busca el tipo y la condición en la base de datos
+    // const tipoEncontrado = await Tipo.findOne({ where: { nombre: tipo } });
+    // const condicionEncontrada = await Condicion.findOne({
+    //   where: { nombre: condicion },
+    // });
+
+    // // Si no se encuentra el tipo o la condición, envía una respuesta de error
+    // if (!tipoEncontrado || !condicionEncontrada) {
+    //   return res.status(404).json({ error: 'Tipo o condición no encontrados' });
+    // }
+
+    // Crea una nueva propiedad en la base de datos
+    const nuevaPropiedad = await Propiedad.create({
+      nombre,
+      descripcion,
+      precio,
+      esDestacado,
+      foto,
+      TipoId: tipoEncontrado.id,
+      CondicionId: condicionEncontrada.id,
+    });
+
+    res.status(201).json({ mensaje: 'Propiedad creada exitosamente', propiedad: nuevaPropiedad });
+  } catch (error) {
+    console.error('Error al crear la propiedad:', error);
+    res.status(500).json({ error: 'Error al crear la propiedad' });
+  }
+});
+
+
+
+
 module.exports = router;
